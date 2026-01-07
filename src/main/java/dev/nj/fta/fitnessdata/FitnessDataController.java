5d727fe4963b0edfdf9cf0069f950303003c36c5
eucarizan <eucarizan@gmail.com>
2026-01-07 21:27:46 +0800

    feat(fitness-data): implement GET /api/tracker endpoint

--- src/main/java/dev/nj/fta/fitnessdata/FitnessDataController.java
+++ src/main/java/dev/nj/fta/fitnessdata/FitnessDataController.java
@@ -4,10 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tracker")
@@ -26,5 +25,12 @@ public class FitnessDataController {
                 fitnessData.getId(), fitnessData.getActivity(), fitnessData.getDuration(), fitnessData.getCalories());
         fitnessDataService.createFitnessData(fitnessData);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FitnessDataResponse>> getAllFitnessData() {
+        logger.info("GET /api/tracker");
+        List<FitnessDataResponse> allData = fitnessDataService.getAllData();
+        return ResponseEntity.status(HttpStatus.OK).body(allData);
     }
 }
