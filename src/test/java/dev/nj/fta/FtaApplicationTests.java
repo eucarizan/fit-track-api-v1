aa07394227d26506955949fba05b1a4b23f0befe
eucarizan <eucarizan@gmail.com>
2026-01-07 16:17:58 +0800

    stage-01 init

---
+++ src/test/java/dev/nj/fta/FtaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nj.fta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
