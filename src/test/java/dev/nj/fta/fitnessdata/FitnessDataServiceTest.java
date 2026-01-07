ddf3566f44d0f40b72931a5400a3622c03b58300
eucarizan <eucarizan@gmail.com>
2026-01-07 20:59:02 +0800

    test(fitness-data): getFitnessData should return list of fitness data, sorted by newly created [RED]

--- src/test/java/dev/nj/fta/fitnessdata/FitnessDataServiceTest.java
+++ src/test/java/dev/nj/fta/fitnessdata/FitnessDataServiceTest.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Sort;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -26,5 +31,23 @@ public class FitnessDataServiceTest {
         fitnessDataService.createFitnessData(data);
 
         verify(fitnessDataRepository).save(data);
+    }
+
+    @Test
+    void getAllFitnessData_returnsListOfFitnessData() {
+        FitnessData data1 = new FitnessData("user-12", "swimming", 950, 320);
+        FitnessData data2 = new FitnessData("user-12", "hiking", 4800, 740);
+
+        when(fitnessDataRepository.findAll(any(Sort.class))).thenReturn(List.of(data2, data1));
+        when(fitnessDataMapper.toResponse(data2)).thenReturn(
+                new FitnessDataResponse(2L, "user-12", "hiking", 4800, 740));
+        when(fitnessDataMapper.toResponse(data1)).thenReturn(
+                new FitnessDataResponse(1L, "user-12", "swimming", 950, 320));
+
+        List<FitnessDataResponse> responseList = fitnessDataService.getAllData();
+
+        assertEquals(2, responseList.size());
+        assertEquals(2L, responseList.get(0).id);
+        assertEquals(1L, responseList.get(1).id);
     }
 }
