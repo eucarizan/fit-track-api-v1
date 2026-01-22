39c3959638d2fa86a772e4d2ee8ac48ecd127991
eucarizan <eucarizan@gmail.com>
2026-01-22 13:45:08 +0800

    test(developer): getDeveloperById returns developerResponse (red)

--- src/test/java/dev/nj/fta/developer/DeveloperServiceTest.java
+++ src/test/java/dev/nj/fta/developer/DeveloperServiceTest.java
@@ -7,6 +7,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -37,5 +39,19 @@ public class DeveloperServiceTest {
 
         verify(developerRepository).save(developer);
         assertEquals(9062L, id);
+    }
+
+    @Test
+    void getDeveloper_byId_returnsDeveloper() {
+        Developer developer = new Developer("johndoe@gmail.com", "qwerty");
+        DeveloperResponse developerResponse = new DeveloperResponse(9062L, "johndoe@gmail.com");
+
+        when(developerRepository.findById(9062L)).thenReturn(Optional.of(developer));
+        when(developerMapper.toResponse(developer)).thenReturn(developerResponse);
+
+        DeveloperResponse response = developerService.getDeveloperById(9062L);
+
+        assertEquals(9062L, response.id());
+        assertEquals("johndoe@gmail.com", response.email());
     }
 }
